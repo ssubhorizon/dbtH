883eb127f29aee0b8c05869d13d4018f8a97da00
Suresh Subramani <118196934+ssubhorizon@users.noreply.github.com>
2024-10-30 19:51:06 -0400

    updated cloud build yaml file

--- models/example/zz_test_model_three.sql
+++
@@ -1,3 +0,0 @@
-select *
-from {{ ref('zz_test_model_one') }}
-where id = 100
--- models/example/zz_test_model_two.sql
+++
@@ -1,6 +0,0 @@
-
--- Use the `ref` function to select from other models
-
-select *
-from {{ ref('zz_test_model_one') }}
-where id = 1
